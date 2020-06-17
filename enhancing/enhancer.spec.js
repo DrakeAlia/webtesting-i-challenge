03b823499aed8f839fe46d4887c5205cbb2b8f70
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-06-17 16:50:34 -0700

    attempted stretch

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,67 +1,88 @@
-const { succeed, fail, repair,  } = require('./enhancer.js');
+const { succeed, fail, repair, get  } = require('./enhancer.js');
 // test away!
 
 describe('enhancer', () => {
+    // accepts an item object and returns a new item with the durability restored to 100
     describe('repair', () => {
-        it('reapir should be able to restore durability to 100', () => {
+        it('should restore durability to 100', () => {
             let item = {
                 name: 'master sword',
                 durability: 27,
                 enhancement: 10
             }
-            const repairedItem = repair(item);
+            const repairedItem = repair(item)
 
-            expect(repairedItem.durability).toBe(100);
-        });
-    });
-
+            expect(repairedItem.durability).toBe(100)
+        })
+    })
+    // accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success
     describe('succeed', () => {
-        it('successful enchacemnet should increase by 1', () => {
-            const enhancementStats = 10
-            const item = {
+        it('should increase enhancement by 1', () => {
+            let enhancementStat = 10
+            let item = {
                 name: 'master sword',
                 durability: 27,
-                enhancement: enhancementStats
+                enhancement: enhancementStat
             }
-            const enhancedItem = succeed(item);
-            expect(enhancedItem.enhancement).toBe(enhancementStats + 1);
-        });
+            const enhancedItem = succeed(item)
+            expect(enhancedItem.enhancement).toBe(enhancementStat + 1)
+        })
         it('should have enhancement stay at 20', () => {
-            const item = {
+            let item = {
                 name: 'master sword',
                 durability: 27,
-                enchacemnet: 20
+                enhancement: 20
             }
-            const enhancedItem = succeed(item);
-            expect(enhancedItem.enchacemnet).toBe(20);
-        });
-    });
-
+            const enhancedItem = succeed(item)
+            expect(enhancedItem.enhancement).toBe(20)
+        })
+    })
+    // accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
     describe('fail', () => {
-        it('durability when failed will decrease by 5', () => {
-            const durabilityStats = 27
-            const item = {
+        it('should decrease durability by 5', () => {
+            let durabilityStat = 27
+            let item = {
                 name: 'master sword',
-                durability: durabilityStats, 
+                durability: durabilityStat,
                 enhancement: 14
             }
             const failedItem = fail(item)
-            expect(failedItem.durability).toBe(durabilityStats - 5);
-        });
-        it('should decrease the durability by 10 and decrease enhancement by 1', () => {
-            const durabilityStats = 27 
-            const enhancementStats = 17 
-            const item = {
+            expect(failedItem.durability).toBe(durabilityStat - 5)
+        })
+        it('should decrease durability by 10 and decrease enchancement by 1', () => {
+            let durabilityStat = 27
+            let enchancementStat = 17
+            let item = {
                 name: 'master sword',
-                durability: durabilityStats, 
-                enhancement: enhancementStats
+                durability: durabilityStat,
+                enhancement: enchancementStat
             }
             const failedItem = fail(item)
-            expect(failedItem.durability).toBe(durabilityStats - 10)
-            expect(failedItem.enhancement).toBe(enhancementStats - 1);
-        });
-    });
-    // describe('get', () => {
-    //     describe('')
-    // })
+            expect(failedItem.durability).toBe(durabilityStat - 10)
+            expect(failedItem.enhancement).toBe(enchancementStat - 1)
+        })
+    })
+    // object that takes an item and returns a new item with the name property
+    describe('get', () => {
+        describe('should modify name with enhancement value', () => {
+            let itemName = 'master sword'
+            let item = {
+                name: itemName,
+                durability: 90,
+                enhancement: 14
+            }
+            const getItem = get(item)
+            expect(getItem.name).toBe(`[+${getItem.enhancement}] ${itemName}`)
+        })
+        describe('should modify name with enhancement value', () => {
+            let itemName = 'master sword'
+            let item = {
+                name: itemName,
+                durability: 90,
+                enhancement: 0
+            }
+            const getItem = get(item)
+            expect(getItem.name).toBe(itemName)
+        })
+    })
 });
